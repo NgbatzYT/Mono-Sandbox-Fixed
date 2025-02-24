b22608eef854424a7b69bf3acac949bf7f179c68
NgbatzYT <94680235+NgbatzYT@users.noreply.github.com>
2025-02-25 07:01:01 +1100

    Update PluginInfo.cs

--- MonoSandbox/PluginInfo.cs
+++ MonoSandbox/PluginInfo.cs
@@ -3,7 +3,7 @@
     public class PluginInfo
     {
         public const string
-            GUID = "homeoverspend.monosandbox", // HOMEOVERSPEND: anagram of both "dev" and "monosphere"
+            GUID = "ngbatz.homeoverspend.monosandbox", // HOMEOVERSPEND: anagram of both "dev" and "monosphere"
             Name = "MonoSandbox",
             Version = "1.2.1";
     }
